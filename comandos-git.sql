6a2e6ed5b221aea4f12a470a38a02d442ead6996
yonelyvan <yonelyvan@gmail.com>
2016-05-09 17:37:15 -0500

    comandos

---
+++ comandos-git.sql
@@ -0,0 +1,18 @@
+git clone 'url'
+git init 
+=====================================
+git add .  				 -- git add filename.txt 
+git commit -m "Commit message"
+git push origin master 			--envio de cambios
+--------------------------------------
+git pull 				--actulaizar ultimo commit 
+==============configuacion==============
+git config --global user.email "correo@gmail.com"
+git config --global user.name "username"
+===================================
+
+
+
+
+
+
